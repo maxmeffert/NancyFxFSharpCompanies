9a92f0a965b7a18d8c0651a4ac0fbe1ef4f86342
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2017-03-25 15:50:03 +0100

    inital commit

---
+++ NancyFxFSharpCompanies/Bootstrapper.fs
@@ -0,0 +1,14 @@
+﻿
+
+namespace FsMvc4MonoSample
+open System
+open Nancy
+
+type Bootstrapper() = 
+    inherit DefaultNancyBootstrapper()
+
+type MyModule() as x =
+    inherit NancyModule()
+
+    do
+        x.Get.["/"] <- fun _ -> box "hello world"
